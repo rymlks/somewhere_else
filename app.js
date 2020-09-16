ed926d25291e720d5d2a8711430ebf6846cc1119
rymlks <rymlks@gmail.com>
2020-09-15 23:28:23 -0400

    Adding node files

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on '+port+'...');
+});
+*/
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
