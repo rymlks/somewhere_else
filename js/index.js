ecbf9003a8f3ed7336b406b22cc49a575b1cc30d
rymlks <rymlks@gmail.com>
2020-11-21 19:24:50 -0500

    Restructure game code. Add ESC key to pause

--- js/index.js
+++ js/index.js
@@ -1,100 +1,26 @@
-import * as THREE from "./three.js/src/Three.js";
-import { handleControls } from "./controls.js";
+import {GameManager} from "./core/GameManager.js";
+import {DevScene} from "./scenes/dev/DevScene.js";
 
-document.body.requestPointerLock();
+var GM = new GameManager();
+GM.scene = new DevScene();
+GM.play();
 
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera4D( 75, window.innerWidth / window.innerHeight, 0.1, 1000, 0.1, 1000 );
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var scene = new THREE.Scene4D();
-
-var bbuf = new THREE.BoxBufferGeometry4D( 0.1, 0.1, 0.1, 1, 1, 1 );
-
-var light = new THREE.AmbientLight4D( 0x404040 ); // soft white light
-light.name = "grey ambient light";
-scene.add( light );
-
-var plight = new THREE.PointLight4D( 0xffffff, 1.0, 100 );
-//plight.add( new THREE.Mesh4D( bbuf, new THREE.MeshBasicMaterial( { color: 0xfedcba } ) ) );
-plight.position.set(3, 3, -3, 3);
-plight.name = "white point light";
-scene.add( plight );
-
-var directionalLight = new THREE.DirectionalLight4D( 0xffffff, 0.25 );
-directionalLight.position.set(-3, -3, -3, -10);
-directionalLight.target = plight;
-directionalLight.name = "white dir light";
-scene.add( directionalLight );
-
-var directionalLight = new THREE.DirectionalLight4D( 0xffffff, 0.25 );
-directionalLight.position.set(3, 3, 3, 10);
-directionalLight.target = plight;
-directionalLight.name = "white dir light 2";
-scene.add( directionalLight );
-
-
-var cubes = [];
-var colors = [
-	0xffffff,
-	0xffff80,
-	0xff80ff,
-	0x80ffff,
-
-	0x808080,
-	0x8080ff,
-	0x80ff80,
-	0xff8080
-]
-
-for (var magnitude = -1; magnitude <= 1; magnitude += 2) {
-	for (var coord of ['x', 'y', 'z', 'w']) {
-		var buff = new THREE.BoxBufferGeometry4D( 2, 2, 2, 1, 1, 1 );
-		var colorslug = 0xff;
-		if (coord === 'w') {
-			colorslug = 0x80;
-		}
-		//var color = colorslug * color_map[coord];
-		var material = new THREE.MeshLambertMaterial( { color: colors.pop() } );
-		var cube = new THREE.Mesh4D(buff, material);
-		cube.position[coord] = magnitude;
-		cube.rotation[coord + 'w'] = 0.5 * Math.PI * magnitude;
-		if (coord + 'w' == 'ww') {
-			cube.rotation['xz'] = Math.PI;
-			cube.rotation['zw'] = Math.PI;
-		}
-		cube.name = "tesscube " + magnitude + coord;
-
-		cubes.push(cube);
-	}
-}
-
-for (cube of cubes) {
-	scene.add(cube);
-}
-
-camera.position.z = 5;
-camera.position.w = 5;
-var orbit = 3;
+// Debug stuff
 
 var infodiv = document.getElementById("info");
-function animate() {
+function updateInfoDiv() {
 	requestAnimationFrame( animate );
 
-	handleControls(scene, camera);
-
-	renderer.render( scene, camera );
-	infodiv.innerHTML  = "x: " + camera.position.x.toPrecision(4) + ", ";
-	infodiv.innerHTML += "y: " + camera.position.y.toPrecision(4) + ", ";
-	infodiv.innerHTML += "z: " + camera.position.z.toPrecision(4) + ", ";
-	infodiv.innerHTML += "w: " + camera.position.w.toPrecision(4);
+	infodiv.innerHTML  = "x: " + GM.camera.position.x.toPrecision(4) + ", ";
+	infodiv.innerHTML += "y: " + GM.camera.position.y.toPrecision(4) + ", ";
+	infodiv.innerHTML += "z: " + GM.camera.position.z.toPrecision(4) + ", ";
+	infodiv.innerHTML += "w: " + GM.camera.position.w.toPrecision(4);
 	infodiv.innerHTML += "<br/>";
-	infodiv.innerHTML += "yz: " + (camera.rotation.yz * 180 / Math.PI).toPrecision(4) + ", ";
-	infodiv.innerHTML += "zx: " + (camera.rotation.zx * 180 / Math.PI).toPrecision(4) + ", ";
-	infodiv.innerHTML += "xy: " + (camera.rotation.xy * 180 / Math.PI).toPrecision(4) + ", ";
-	infodiv.innerHTML += "xw: " + (camera.rotation.xw * 180 / Math.PI).toPrecision(4) + ", ";
-	infodiv.innerHTML += "yw: " + (camera.rotation.yw * 180 / Math.PI).toPrecision(4) + ", ";
-	infodiv.innerHTML += "zw: " + (camera.rotation.zw * 180 / Math.PI).toPrecision(4);
+	infodiv.innerHTML += "yz: " + (GM.camera.rotation.yz * 180 / Math.PI).toPrecision(4) + ", ";
+	infodiv.innerHTML += "zx: " + (GM.camera.rotation.zx * 180 / Math.PI).toPrecision(4) + ", ";
+	infodiv.innerHTML += "xy: " + (GM.camera.rotation.xy * 180 / Math.PI).toPrecision(4) + ", ";
+	infodiv.innerHTML += "xw: " + (GM.camera.rotation.xw * 180 / Math.PI).toPrecision(4) + ", ";
+	infodiv.innerHTML += "yw: " + (GM.camera.rotation.yw * 180 / Math.PI).toPrecision(4) + ", ";
+	infodiv.innerHTML += "zw: " + (GM.camera.rotation.zw * 180 / Math.PI).toPrecision(4);
 }
-animate();
+updateInfoDiv();
--- js/init.js
+++
@@ -1,7 +0,0 @@
-import { keyPressed, keyReleased, mouseMoved, wheelScrolled } from "./controls.js"
-
-// Set up event handlers for controls
-window.onkeyup = keyReleased;
-window.onkeydown = keyPressed;
-document.onmousemove = mouseMoved;
-document.onwheel = wheelScrolled;
